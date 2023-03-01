faf5d0991d5e6cbd8e8423c6ec6b8441cfbd2134
Zafnakhairunnisa <zafna.khairunnisa@mail.ugm.ac.id>
2023-03-01 20:24:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fragmentstatisdinamis/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.fragmentstatisdinamis;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private TextView productInfo;
+    private boolean isFragmentDisplayed = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        productInfo = findViewById(R.id.product_info);
+
+        mButton = findViewById(R.id.open_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isFragmentDisplayed) {
+                    displayFragment();
+                } else {
+                    closeFragment();
+                }
+            }
+        });
+    }
+
+    private void displayFragment() {
+
+        SimpleFragment simpleFragment = SimpleFragment.newInstance();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.fragment_container, simpleFragment).addToBackStack(null).commit();
+        mButton.setText("Mantap!");
+        productInfo.setVisibility(View.GONE);
+        isFragmentDisplayed = true;
+
+    }
+
+    private void closeFragment() {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        SimpleFragment simpleFragment = (SimpleFragment) fragmentManager.findFragmentById(R.id.fragment_container);
+        if (simpleFragment != null) {
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(simpleFragment).commit();
+            mButton.setText("Lihat Kelebihan");
+            productInfo.setVisibility(View.VISIBLE);
+            isFragmentDisplayed = false;
+        }
+
+    }
+}
